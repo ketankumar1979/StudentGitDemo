0a2577a19943937a6bacfbff51e2f0a6707f9750
ketankumar <ketanicengg@yahoo.com>
2020-05-23 22:49:00 +0100

    Modified changes

--- src/test/java/GitPractice/RunCukeTest.java
+++ src/test/java/GitPractice/RunCukeTest.java
@@ -5,8 +5,11 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/Resource/HomePage.feature")
-
+@CucumberOptions(features = "src/test/Resource/HomePage.feature",
+        plugin = {//"html:target/cucumber-html-report",
+                //"junit:target/junit-report.xml"})
+                "json:target/cucumber-report.json"})
+               // "pretty:target/cucumber-pretty.txt"})
 
 public class RunCukeTest {
 }
